d17c42561a03e7861205a824c6bda8a692872c6d
abdi <abdiakhmet.1.618@gmail.com>
2024-06-18 22:29:33 +0500

    presentation data classes created

--- app/src/main/java/com/weather_app/weather_forecast/domain/data_classes/WeatherData.kt
+++ app/src/main/java/com/weather_app/weather_forecast/domain/data_classes/WeatherData.kt
@@ -5,14 +5,46 @@ import android.health.connect.datatypes.units.Temperature
 data class WeatherData(
 
     val currentWeatherData: CurrentWeatherData,
-    val hourlyWeatherData: HourlyWeatherData
+    // map{ Day - { Hour - WeatherData }}
+    val hourlyWeatherData: Map<Int, Map<Int, HourlyWeatherData>> ,
+    // list{ Day - WeatherData}
+    val dailyWeatherData: List<DailyWeatherData>
 )
 
 
 data class CurrentWeatherData(
-    val temperature: Int
+    val time: String,
+    val temperature: Int,
+    val humidity: Int,
+    val apparentTemperature: Int,
+    val precipitation: Int,
+    val weatherCode: Int,
+    val cloudCover: Int,
+    val pressure: Int,
+    val windSpeed: Int,
+    val windGustsSpeed: Int
 )
 
 data class HourlyWeatherData (
-    val temperature: Int
+    val time: String,
+    val temperature: Int,
+    val humidity: Int,
+    val apparentTemperature: Int,
+    val precipitation: Int,
+    val weatherCode: Int,
+    val cloudCover: Int,
+    val windSpeed: Int,
+)
+
+data class DailyWeatherData (
+    val time: String,
+    val weatherCode: Int,
+    val maxTemperature: Int,
+    val minTemperature: Int,
+    val sunrise: String,
+    val sunset: String,
+    val uvIndex: Int,
+    val windSpeed: Int,
+    val windGustsSpeed: Int,
+    val windDirection: String,
 )
